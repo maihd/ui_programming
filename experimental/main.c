8e20b5968d197dd9ee5c2894be58d329485282f8
maihd <maihd.dev@gmail.com>
2024-01-23 18:21:59 +0700

    add experimental

---
+++ experimental/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main()
+{
+    printf(
+        "Hello world!\n"
+        "This is a project for experimental some features that doesnot very needed for UI programming.\n"
+        "But having is a plus.\n");
+    return 0;
+}
+
+//! EOF
+
+
